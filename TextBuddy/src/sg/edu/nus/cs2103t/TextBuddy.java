a81f26e92afef318f431a64f9d26c9733bc3d8fd
Jing Yin <ongjingyin@yahoo.com.sg>
2015-09-19 01:24:03 +0800

    add textbuddy

---
+++ TextBuddy/src/sg/edu/nus/cs2103t/TextBuddy.java
@@ -0,0 +1,286 @@
+package sg.edu.nus.cs2103t;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
+public class TextBuddy {
+	//messages shown to users
+	private static final String MESSAGE_BEGIN = "Welcome to TextBuddy. %1$s is ready for use";
+	private static final String MESSAGE_ADD = "added to %1$s: '%2$s'";
+	private static final String MESSAGE_DELETE = "deleted from %1$s: '%2$s'";
+	private static final String MESSAGE_CLEAR = "all content deleted from %1$s";
+	private static final String MESSAGE_EMPTY = "%1$s is empty";
+	private static final String MESSAGE_INVALID = "invalid command format: %1$s";
+		
+
+	//possible command types
+	enum COMMAND_TYPE{
+		ADD_TEXT, DELETE_TEXT, CLEAR_TEXT, DISPLAY_TEXT, INVALID, EXIT;
+	}
+	
+	//data structure for storing texts
+	public static class ListNode{
+		public ListNode nextText;
+		public String content;
+	}
+	
+	private static ListNode headText = new ListNode();
+	
+	private static Scanner scanner = new Scanner(System.in);
+	
+	private static String textFileName;
+	
+	public static void main(String[] args){
+		textFileName = args[0];
+				
+		showUser(String.format(MESSAGE_BEGIN, textFileName));
+		while (true) {
+			System.out.print("command: ");
+			String command = scanner.nextLine();
+			String userCommand = command;
+			String feedback = runCommand(userCommand);
+			showUser(feedback);
+		}
+	}
+	
+	private static void showUser(String text){
+		System.out.println(text);
+	}
+	
+	public static String runCommand(String userInput){
+		if (userInput.trim().equals(""))
+			return String.format(MESSAGE_INVALID, userInput);
+		
+		COMMAND_TYPE command = getCommand(userInput);
+		
+		switch (command){
+		case ADD_TEXT:
+			return addText(userInput);
+		case DELETE_TEXT:
+			return deleteText(userInput);
+		case CLEAR_TEXT:
+			return clearText();
+		case DISPLAY_TEXT:
+			return displayText();
+		case INVALID:
+			return String.format(MESSAGE_INVALID, userInput);
+		case EXIT:
+			writeIntoFile(displayText());
+			System.exit(0);
+		default:
+			throw new Error("Unrecognized command type");
+		}
+	}
+	
+	/**
+	 * This operation determines which of the supported command types the user
+	 * wants to perform
+	 * @param userInput
+	 * 		the full string the user entered as the command
+	 * @return the command type
+	 */
+	private static COMMAND_TYPE getCommand(String userInput){
+		String command = getFirstWord(userInput);
+		
+		if (command == null){
+			throw new Error("Command cannot be empty");
+		}
+		
+		if (command.equalsIgnoreCase("add")){
+			return COMMAND_TYPE.ADD_TEXT;
+		} else if (command.equalsIgnoreCase("delete")){
+			return COMMAND_TYPE.DELETE_TEXT;
+		} else if (command.equalsIgnoreCase("clear")){
+			return COMMAND_TYPE.CLEAR_TEXT;
+		} else if (command.equalsIgnoreCase("display")){
+			return COMMAND_TYPE.DISPLAY_TEXT;
+		} else if (command.equalsIgnoreCase("exit")){
+			return COMMAND_TYPE.EXIT;
+		} else{
+			return COMMAND_TYPE.INVALID;
+		}
+	}
+
+	
+	/**
+	 * This operation is used to add text input from the user into the program
+	 * @param userInput
+	 * 		the full string the user entered as the command
+	 * @return message to indicate that text has been added successfully
+	 */
+	private static String addText(String userInput){
+		String textToAdd = removeFirstWord(userInput);
+		if (isEmpty(headText)){
+			if (headText == null){
+				headText = new ListNode();
+			}
+			headText.content = textToAdd;
+		} else {
+			ListNode newText = new ListNode();
+			newText.content = textToAdd;
+			ListNode lastText = findLastText();
+			lastText.nextText = newText;
+		}
+		
+		return String.format(MESSAGE_ADD, textFileName, textToAdd);
+	}
+	
+	/**
+	 * @return the last text node in the program
+	 */
+	private static ListNode findLastText(){
+		ListNode curr = headText;
+		while (!isEmpty(curr.nextText)){
+			curr = curr.nextText;
+		}
+		return curr;
+	}
+	
+	/**
+	 * This operation is used to delete texts corresponding to the line number indicated
+	 * by the user
+	 * @param userInput
+	 * 		the full string the user entered as the command
+	 * @return message to indicate success or failure of the delete operation
+	 */
+	private static String deleteText(String userInput){
+		
+		int deletePosition = positionToRemove(userInput);
+		String deletedText;
+		
+		if (deletePosition < 0){
+			return String.format(MESSAGE_INVALID, userInput);
+		}
+		
+		if (isEmpty(headText)){
+			return String.format(MESSAGE_EMPTY, textFileName);
+		} else if (deletePosition == 0){
+			deletedText = getText(headText);
+			headText = headText.nextText;
+			return String.format(MESSAGE_DELETE, textFileName, deletedText);
+		}
+		
+		ListNode textBeforeDeletePosition = textBeforeDeletedText(headText, deletePosition);
+		
+		if (isEmpty(textBeforeDeletePosition) || isEmpty(textBeforeDeletePosition.nextText)){
+			return String.format(MESSAGE_INVALID, userInput);
+		}
+		
+		ListNode textNodeToDelete = textBeforeDeletePosition.nextText;
+		
+		deletedText = getText(textNodeToDelete);
+		textBeforeDeletePosition.nextText = textNodeToDelete.nextText;
+		return String.format(MESSAGE_DELETE, textFileName, deletedText);
+	}
+	
+	/**
+	 * @param textNode
+	 * @param count
+	 * @return the text node prior to the text node being deleted
+	 */
+	private static ListNode textBeforeDeletedText(ListNode textNode, int count) {
+		while ((count > 1) && (textNode != null)){
+			textNode = textNode.nextText;
+			count--;
+		}
+		return textNode;
+	}
+	
+	/**
+	 * @param textNode
+	 * @return text contained within a text node
+	 */
+	private static String getText(ListNode textNode) {
+		return textNode.content;
+	}
+	
+	/**
+	 * @param textNode
+	 * @return a boolean value indicating whether a text node is empty
+	 */
+	private static boolean isEmpty(ListNode textNode) {
+		return (textNode == null) || (textNode.content == null);
+	}
+	
+	/**
+	 * @param userInput
+	 * @return the line number indicated by the user to remove from the text file
+	 */
+	private static Integer positionToRemove(String userInput){
+		String number = removeFirstWord(userInput);
+		try{
+			return Integer.parseInt(number) - 1;
+		} catch (NumberFormatException e){
+			return -1;
+		}
+	}
+	
+	/**
+	 * This operation deletes all text in the text file
+	 * @return message indicating successful clearance
+	 */
+	private static String clearText(){
+		headText.content = null;
+		headText.nextText = null;
+		return String.format(MESSAGE_CLEAR, textFileName);
+	}
+	
+	
+	/**
+	 * @return text contained within the text file
+	 */
+	private static String displayText(){
+		ListNode textNode = headText;
+		String allText = "", textLine;
+		int lineCount = 1;
+
+		while (!isEmpty(textNode)){
+			textLine = formatTextLine(lineCount, textNode);
+			allText += textLine;
+			textNode = textNode.nextText;
+			lineCount++;
+		}
+		
+		if (allText == ""){
+			return String.format(MESSAGE_EMPTY, textFileName);
+		}
+		return allText;
+	}
+	
+	private static String formatTextLine(int lineCount, ListNode textNode) {
+		return Integer.toString(lineCount) + ". " + getText(textNode) + "\r\n";
+	}
+	
+	private static void writeIntoFile(String text){
+		try{
+			File file = new File(findPathName());
+			if (!file.exists()){
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(text);
+			bw.close();
+		} catch (IOException e){
+			System.out.println("ERROR: Unable to write into file");
+		}
+	}
+	
+	private static String findPathName() {
+		return System.getProperty("user.dir")+"/"+textFileName;
+	}
+	
+	private static String removeFirstWord(String line){
+		return line.replace(getFirstWord(line), "").trim();
+	}
+	
+	private static String getFirstWord(String line){
+		return line.trim().split("\\s+")[0];
+	}
+	
+}
